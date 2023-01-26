a6b31c448e8e6f8619b924ecd5ec3677b4a3096e
Byron <byron.southerland@icloud.com>
2023-01-26 00:03:15 -0500

    Added row grouping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ColDef, RowGroupingDisplayType } from 'ag-grid-community';
+import 'ag-grid-enterprise';
+//import { Columns, DefaultColDef } from './grid_defs/deployData';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app-deploy-ng';
+export class AppComponent implements OnInit {
+   title = 'app-deploy-ng';
+
+   Columns: ColDef[] = [
+      { field: 'project', rowGroup: true, hide: true },
+      { field: 'deployEnv', rowGroup: true, hide: true },
+      { field: 'tagName' },
+      { field: 'branch' },
+      { field: 'version' },
+      { field: 'timestamp' },
+   ];
+
+   DefaultColDef: ColDef = {
+      flex: 1,
+      minWidth: 100,
+      sortable: true,
+      resizable: true,
+   };
+
+   groupDisplayType: RowGroupingDisplayType = 'multipleColumns';
+   gridOptions = {
+      columnDefs: this.Columns,
+      defaultColDef: this.DefaultColDef,
+      animateRow: true,
+      autoGroupColumnDef: {
+         minWidth: 200,
+         headerName: 'Projects',
+         cellRendererParams: {
+            suppressCount: true,
+            innerRenderer: (p: { value: string }) => '<b>' + p.value + '</b>',
+         },
+      },
+   };
+
+   rowData$?: Observable<any[]>;
+   columnDefs = this.Columns;
+   defaultColDef = this.DefaultColDef;
+   rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' = 'never';
+
+   constructor(private http: HttpClient) {}
+
+   ngOnInit(): void {
+      this.rowData$ = this.http.get<any[]>(
+         'https://httpappdeployment.azurewebsites.net/api/httpappdeploymentout'
+      );
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent],
+   imports: [BrowserModule, AgGridModule, HttpClientModule],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
